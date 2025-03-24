f63454d66601845287a698228c49e44d8b2d99ac
Gideonngige <ushindigideon01@gmail.com>
2025-03-25 01:19:14 +0300

    added Users model

--- complain_compliment_app/models.py
+++ complain_compliment_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=100)
+    ROLES = (
+        ('admin', 'admin'),
+        ('lecturer', 'lecturer'),
+        ('staff', 'staff'),
+        ('student', 'student'),
+    )
+    role = models.CharField(max_length=100, choices=ROLES)
+    password = models.CharField(max_length=100)
