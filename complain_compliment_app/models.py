34589ffcbf2f5b27af761e57a66aadd2dabf2049
Gideonngige <ushindigideon01@gmail.com>
2025-03-26 06:47:06 +0300

    added Feedback model

--- complain_compliment_app/models.py
+++ complain_compliment_app/models.py
@@ -12,3 +12,38 @@ class Users(models.Model):
     )
     role = models.CharField(max_length=100, choices=ROLES)
     password = models.CharField(max_length=100)
+
+class Feedbacks(models.Model):
+    feedback_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    message = models.TextField()
+    CATEGORY = (
+        ('academic', 'academic'),
+        ('finance', 'finance'),
+        ('hostel', 'hostel'),
+        ('library', 'library'),
+        ('medical', 'medical'),
+        ('security', 'security'),
+        ('transport', 'transport'),
+        ('complaint', 'complaint'),
+    )
+    category = models.CharField(max_length=100, choices=CATEGORY)
+    STATUS = (
+        ('pending', 'pending'),
+        ('on-progress', 'on-progress'),
+        ('resolved', 'resolved'),
+    )
+    status = models.CharField(max_length=100, choices=STATUS)
+    date
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            original = Feedbacks.objects.get(pk=self.pk)
+            if original.status != self.status:
+                from django.utils.timezone import now
+                self.updated_at = now()
+        super().save(*args, **kwargs)
+
+
